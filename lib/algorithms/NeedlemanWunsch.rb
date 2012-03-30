c1e49e468950f121a1660410d9bcead36f951026
Bjoern Rennhak <bjoern@rennhak.com>
2012-03-30 21:54:46 +0900

    - Changes and experiments

--- lib/algorithms/NeedlemanWunsch.rb
+++ lib/algorithms/NeedlemanWunsch.rb
@@ -1,6 +1,16 @@
 #!/usr/bin/ruby19
 
 
+#
+# Needleman, Saul B.; and Wunsch, Christian D. (1970). "A general method
+# applicable to the search for similarities in the amino acid sequence of two
+# proteins". Journal of Molecular Biology 48 (3): 443–53.
+# doi:10.1016/0022-2836(70)90057-4. PMID 5420325.
+#
+
+
+
+
 # Necessary libraries
 require 'narray'
 
@@ -28,6 +38,8 @@ class NeedlemanWunsch
   #
   # FIXME:      These values should be mapped out to a config file etc.
   def init_genetic
+
+    puts "-> Genetic"
 
     gap = -5
 
@@ -66,45 +78,36 @@ class NeedlemanWunsch
   # FIXME:      These values should be mapped out to a config file etc.
   def init_generic
 
+    puts "Generic"
+
     gap = -2
 
-    similarity = {  'AA' => 10,
-                    'AG' => -1,
-                    'AC' => -3,
-                    'AT' => -4,
-                    'GA' => -1,
-                    'GG' =>  7,
-                    'GC' => -5,
-                    'GT' => -3,
-                    'CA' => -3,
-                    'CG' => -5,
-                    'CC' =>  9,
-                    'CT' =>  0,
-                    'TA' => -4,
-                    'TG' => -3,
-                    'TC' =>  0,
-                    'TT' =>  8 }
+    similarity = {}
+
+    # All permutations e.g. 01, 10 etc. are penalized
+    ["0", "1", "2", "3", "4", "5", "6", "7"].permutation(2) do |x|
+      similarity[ x.join( "" ).to_s ] = -1
+    end
+
+    # Matches "00" and same letter matches .. similar are awarded
+    ["0", "1", "2", "3", "4", "5", "6", "7"].each do |x|
+      similarity[ ( x + x ).to_s ] = 1
+    end
 
     return [ gap, similarity ]
   end # of def init_genetic # }}}
 
 
+  # Inspired by: http://snippets.dzone.com/posts/show/2199    -> credits: to ruby bioinformatics alignment by "raaum" on "Thu Jun 15 15:18:08 -0400 2006"
   def needle sequence, reference
 
     gap = @gap
-    d = @similarity
+    s = @similarity
 
-    s = {}
-
-    # All permutations e.g. 01, 10 etc. are penalized
-    ["0", "1", "2", "3", "4", "5", "6", "7"].permutation(2) do |x|
-      s[ x.join( "" ).to_s ] = -1
-    end
-
-    # Matches "00" and same letter matches .. similar are awarded
-    ["0", "1", "2", "3", "4", "5", "6", "7"].each do |x|
-      s[ ( x + x ).to_s ] = 1
-    end
+    puts "Gap: #{gap.to_s}"
+    puts "Similarity Matrix: "
+    p s
+    puts ""
 
     rows = reference.length + 1
     cols = sequence.length + 1
@@ -115,10 +118,10 @@ class NeedlemanWunsch
     for j in 0...(cols) do a[0,j] = 0 end
     for i in 1...(rows)
       for j in 1...(cols)
-        sec = s[ (reference[i-1].chr + sequence[j-1].chr) ]
-        sec = 0 if( sec.nil? )
-        choice1 = a[i-1, j-1] + sec
-        # choice1 = a[i-1, j-1] + s[ (reference[i-1].chr + sequence[j-1].chr) ]
+        #sec = s[ (reference[i-1].chr + sequence[j-1].chr) ]
+        #sec = 0 if( sec.nil? )
+        # choice1 = a[i-1, j-1] + sec
+        choice1 = a[i-1, j-1] + s[ (reference[i-1].chr + sequence[j-1].chr).upcase ]
 
         choice2 = a[i-1, j] + gap
         choice3 = a[i, j-1] + gap
@@ -180,28 +183,47 @@ end # of class NeedlemanWunsch # }}}
 
 if __FILE__ == $0
 
-  nw          = NeedlemanWunsch.new( nil, true )
+  nw          = NeedlemanWunsch.new( nil, false )
 
-  sequence    = "gccctagcg"
-  reference   = "gcgcaatg"
+  # sequence    = "AGACTAGTTAC"
+  # reference   = "CGAGACGT"
+  # result of "AGACTAGTTAC" and  "CGAGACGT" should be "CGA---GACGT" according to wikipedia, but we get "CGAGAC--G-T--"
+
+  # sequence      = "001110111"
+  # reference     = "0012210122211"
+
+  sequence        = File.open( "../../data/cycle10_directions.gpdata", "r" ).readlines.collect!{ |l| l.strip }.join( "" )
+  reference        = File.open( "../../data/cycle2_directions.gpdata", "r" ).readlines.collect!{ |l| l.strip }.join( "" )
 
   s, r =  nw.needle(sequence, reference)
+
   ns   = []
   0.upto( s.length - 1 ).each do |n|
     ns << n.to_s
   end
 
-  ss = ns.zip( s.split( "" ) )
-  ss.collect!{ |array| array.join( " " ) }
-  ss = ss.join( "\n" )
+  #ss = ns.zip( s.split( "" ) )
+  ss = s
+  #ss.collect!{ |array| array.join( " " ) }
+  #ss = ss.join( "\n" )
 
-  rs = ns.zip( r.split( "" ) )
-  rs.collect!{ |array| array.join( " " ) }
-  rs = rs.join( "\n" )
+  #rs = ns.zip( r.split( "" ) )
+  rs = r
+  #rs.collect!{ |array| array.join( " " ) }
+  #rs = rs.join( "\n" )
 
 
   #puts ss
-  puts rs
+  #p ss
+  #p rs
+
+  final_ss = []
+  0.upto( rs.length - 1 ) do |n|
+    final_ss << [ n.to_s + " " + rs[n].to_s ]
+  end
+  puts final_ss.join( "\n" )
+
+  
 
 end
 
